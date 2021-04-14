3a31243d0ac3484108ee8095971bb7bb12885793
juanpablo-estrada <jpulgaestrada@gmail.com>
2021-04-14 12:27:08 -0500

    Cambio de texto

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <p>Hola Mundo</p>
+        <p>Prueba de AWS Amplify</p>
         <a
           className="App-link"
           href="https://reactjs.org"
